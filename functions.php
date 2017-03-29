6322ad4304ba2617144cfd80e8ec3fb5a9c68af6
Eli <eliscolatour@gmail.com>
2017-03-29 13:19:24 +0100

    Add constant to the theme functions.
    Constants for the path of theme's root, images, css, javascript
    folders.

--- functions.php
+++ functions.php
@@ -1,6 +1,31 @@
 <?php
 /* 
  * =====================================================
- *   Afrique Televison: functions.php Template file
+ *
+ * functions.php 
+ * 
+ * The Theme's Functions
+ * 
  * =====================================================
  */
+
+
+/*------------------------------------------------------
+ * 
+ * 1 - CONSTANTS 
+ * 
+ * ------------------------------------------------------
+ */ 
+
+// A constant for the path of the theme root
+define( 'THEME_ROOT', get_stylesheet_directory_uri() );
+
+// A constant for the path of the theme default images folder
+define( 'IMAGES', THEME_ROOT.'/images' );
+
+// A constant for the path of thee theme default css folder 
+define( 'CSS', THEME_ROOT.'/css' );
+
+// A constant for the path of the theme default Javascript folder
+define( 'JS', THEME_ROOT.'/js' );
+
