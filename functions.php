3f59c58bd3faf9534cbb7cf851691dcd1f0fd355
Eli <eliscolatour@gmail.com>
2017-03-29 14:32:19 +0100

    Reload the theme Setup

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
-/* 
+
+/*
  * =====================================================
  *
  * functions.php 
@@ -10,54 +11,56 @@
  */
 
 
-/*------------------------------------------------------
+/* ------------------------------------------------------
  * 
  * 1 - CONSTANTS 
  * 
  * ------------------------------------------------------
- */ 
+ */
 
 // A constant for the path of the theme root
-define( 'THEME_ROOT', get_stylesheet_directory_uri() );
+define('THEME_ROOT', get_stylesheet_directory_uri());
 
 // A constant for the path of the theme default images folder
-define( 'IMAGES', THEME_ROOT.'/images' );
+define('IMAGES', THEME_ROOT . '/images');
 
 // A constant for the path of thee theme default css folder 
-define( 'CSS', THEME_ROOT.'/css' );
+define('CSS', THEME_ROOT . '/css');
 
 // A constant for the path of the theme default Javascript folder
-define( 'JS', THEME_ROOT.'/js' );
+define('JS', THEME_ROOT . '/js');
 
 
-/*------------------------------------------------------
+/* ------------------------------------------------------
  * 
  * 2 - THEME SETUP
  * 
  * ------------------------------------------------------
  */
 
-if( !function_exists( 'aftv_theme_setup' ) ){
-    
-    function aftv_theme_setup(){
+if (!function_exists('aftv_theme_setup')) {
+
+    function aftv_theme_setup() {
         // Make the theme available for translation
-        $lang_dir = THEME_ROOT.'/languages';
-        load_textdomain( 'afriqtv', $lang_dir );
-        
-        
+        $lang_dir = THEME_ROOT . '/languages';
+        load_textdomain('afriqtv', $lang_dir);
+
+
         // Enable support for Post Formats
-        $aftv_supported_format = array (
+        $aftv_supported_format = array(
             'image',
             'video',
             'gallery',
             'audio',
         );
-        add_theme_support( 'post-format', $aftv_supported_format );
-        
+        add_theme_support('post-format', $aftv_supported_format);
+
         // Register Navigation Menus
-        register_nav_menus( array(
-            'aftv-header-menu' => __( 'Menu principal', 'afriqtv' )
+        register_nav_menus(array(
+            'aftv-header-menu' => __('Menu principal', 'afriqtv')
         ));
     }
-    
+
+    //Load Aftv theme setup after Wordpress theme setup
+    add_action('after_theme_setup', 'aftv_theme_setup');
 }
