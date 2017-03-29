e0e745be80a2d63032477b943ec9e2ca3beba82f
Eli <eliscolatour@gmail.com>
2017-03-29 12:28:00 +0100

    Add the header.php head section

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * =====================================================
+ *   Afrique Televison: header.php Template file
+ * =====================================================
+ */
+?>
+<!DOCTYPE html >
+<html <?php language_attributes(); ?> >
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>" />
+        <meta name="viewport" value="width=device-wdith, initial-scale=1.0" />
+        <link rel="profile" href="" />
+
+        <?php wp_head(); ?>
+    </head>
+    <body class="<?php body_class(); ?>" >
+        
+    </body>
+</html>
