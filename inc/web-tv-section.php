dccc4197f15ae165ad3927a8003bcba429f6e701
Eli <eliscolatour@gmail.com>
2017-04-02 13:08:05 +0100

    Create an entire file for the web Tv section

---
+++ inc/web-tv-section.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Display The web Tv section
+ */
+?>
+<div id="web_tv" class="mdl-grid aftv-web_tv">
+    <div id="web_tv-player" class="aftv-player mdl-cell mdl-cell--12-col">
+        <div class="aftv-media">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PL7-8vqQVPcSfrR6R97L8RTk5nHRYWmOxt" frameborder="0" allowfullscreen></iframe>
+        </div>
+    </div>
+</div>
