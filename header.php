2add82d559762622f330150a66fe351e2e83def1
Eli <eliscolatour@gmail.com>
2017-04-02 13:06:53 +0100

    Adjust Post title font size

--- header.php
+++ header.php
@@ -87,13 +87,3 @@
                 }
                 ?>
             </div>
-            <main class="mdl-layout__content">
-                <!--<div class="mdl-grid aftv-content_spacer"></div>-->
-                <div class="page-content">
-                    <div id="web_tv" class="mdl-grid aftv-web_tv">
-                        <div id="web_tv-player" class="aftv-player mdl-cell mdl-cell--12-col">
-                            <div class="aftv-media">
-                                <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PL7-8vqQVPcSfrR6R97L8RTk5nHRYWmOxt" frameborder="0" allowfullscreen></iframe>
-                            </div>
-                        </div>
-                    </div>
