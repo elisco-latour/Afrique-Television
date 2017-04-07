60f50c06f4cffa081db3bed00f3343382d733d93
Eli <eliscolatour@gmail.com>
2017-04-07 14:42:36 +0100

    Add a page template do display Wordpress page post type

--- page.php
+++ page.php
@@ -1,3 +1,38 @@
+<?php
+/*
+ * Single pageTemplate
+ */
+?>
 <?php get_header(); ?>
+<?php while (have_posts()):the_post(); ?>
+    <div class="aftv-ribbon"></div>
+    <main class="mdl-layout__content aftv-single--page">
+        <div class="page-content ">
 
-<?php get_footer(); ?>
+            <?Php
+            /*
+             * Load Web TV Section
+             */
+            require get_template_directory() . '/inc/web-tv-section.php';
+            ?>
+            <div class="mdl-grid  aftv-single--page-container">
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--hide-tablet mdl-cell--hide-phone"></div>
+                <article class="mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--8-col">
+
+                    <div class="aftv-single--posst-content">
+                        <header class="aftv-single--post-header">
+                            <h1 class="aftv-post__title mdl-card__title-text mdl-card__title-text"><?php the_title(); ?></h1>
+                            <div class="aftv-post__meta-data">
+                                <p class="aftv-single--page__metadata">
+                                    <span class="aftv-cat"><?php the_category(' | '); ?></span>
+                                    <span class="aftv-post__date"><?php _e('publié le: ') ?><?php the_time('j, F, Y'); ?></span>
+                                </p>
+                            </div>
+                        </header>
+                        <?php the_content(); ?>
+                    </div>
+                </article>
+            </div>
+        <?php endwhile; ?>
+        <?php get_footer(); ?>
+
