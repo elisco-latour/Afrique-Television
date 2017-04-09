0cdcab39b8d9f1465461b113f05f72df7a75d8e0
Eli <eliscolatour@gmail.com>
2017-04-09 01:15:07 +0100

    Add wordpress posts content like embed or images default values

--- single.php
+++ single.php
@@ -4,17 +4,32 @@
  */
 ?>
 <?php get_header(); ?>
-<?php while (have_posts()):the_post(); ?>
-    <div class="aftv-ribbon"></div>
-    <main class="mdl-layout__content aftv-single--page">
-        <div class="page-content ">
+<div class="aftv-ribbon"></div>
+<main class="mdl-layout__content aftv-single--page">
+    <div class="page-content ">
 
-            <?Php
-            /*
-             * Load Web TV Section
-             */
-            require get_template_directory() . '/inc/web-tv-section.php';
-            ?>
+        <?Php
+        /*
+         * Load Web TV Section
+         */
+        require get_template_directory() . '/inc/web-tv-section.php';
+        ?>
+        <?php while 
+            (have_posts()):the_post();
+        
+            $aftv_page_links = array(
+                'before' => '<p>' . __('Pages:', 'afrique-television'),
+                'after' => '</p>',
+                'link_before' => '',
+                'link_after' => '',
+                'next_or_number' => 'number',
+                'separator' => ' ',
+                'nextpagelink' => __('Next page', 'afrique-television'),
+                'previouspagelink' => __('Previous page', 'afrique-television'),
+                'pagelink' => '%',
+                'echo' => 1
+            );
+        ?>
             <div class="mdl-grid  aftv-single--page-container">
                 <div class="mdl-cell mdl-cell--2-col mdl-cell--hide-tablet mdl-cell--hide-phone"></div>
                 <article id="post-<?php the_ID(); ?>" <?php post_class("mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--8-col"); ?>>
@@ -28,14 +43,20 @@
                             <div class="aftv-post__meta-data">
                                 <p class="aftv-single--page__metadata">
                                     <span class="aftv-cat"><?php the_category(' | '); ?></span>
-                                    <span class="aftv-post__date"><?php _e('publi&eacute; le: ','afrique-television') ?><?php the_time('j, F, Y'); ?></span>
+                                    <span class="aftv-post__date"><?php _e('publi&eacute; le: ', 'afrique-television') ?><?php the_time('j, F, Y'); ?></span>
                                 </p>
                             </div>
                         </header>
                         <?php the_content(); ?>
                     </div>
                 </article>
-            </div>
+            
+            <?php
+            // Page links configuration
+
+            wp_link_pages($aftv_page_links);
+            ?>
+        </div>
         <?php endwhile; ?>
         <?php get_footer(); ?>
 
